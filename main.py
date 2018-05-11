da687e62d916af124b389c53d2d64f4043d58609
Daniel Cole <daniel@judgezarbi.com>
2018-05-12 00:49:56 +0100

    Tidy up the navigation code a bit. Closes #43

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ import exports
 import imports
 
 
+def _export(save_file):
+    success = False
+    while not success:
+        print('What do you wish to export?')
+        print('1. Team')
+        print('2. Team pack')
+        decision = input('--> ')
+        if (decision.strip() == '1'):
+            success = True
+            exports.team.export_team()
+        elif (decision.strip() == '2'):
+            success = True
+            exports.pack.create_team_pack()
+        else:
+            print('You did not choose a valid option. Please try again.')
+
+
+def _import(save_file):
+    success = False
+    while not success:
+        print('What do you wish to import?')
+        print('1. Team or team pack')
+        decision = input('--> ')
+        if (decision.strip() == '1'):
+            success = True
+            imports.team.import_team(save_file)
+            util.save.save_data(save_file)
+        else:
+            print('You did not choose a valid option. Please try again.')
+
+
 def main():
 
     print("Ferrea's SMB2 Team Transfer Tool v0.3 beta")
@@ -16,18 +47,13 @@ def main():
         print('Do you wish to import or export data?')
         print('1. Import')
         print('2. Export')
-        print('3. Create team pack')
         decision = input('--> ')
         if (decision.strip() == '1'):
             success = True
-            imports.team.import_team(save_file)
-            util.save.save_data(save_file)
+            _import(save_file)
         elif (decision.strip() == '2'):
             success = True
-            exports.team.export_team()
-        elif (decision.strip() == '3'):
-            success = True
-            exports.pack.create_team_pack()
+            _export(save_file)
         else:
             print('You did not choose a valid option. Please try again.')
 
