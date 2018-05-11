b04032bc1241dab046b97d971f1966e43d6e2e1b
Daniel Cole <daniel@judgezarbi.com>
2018-05-11 23:53:05 +0100

    Move and refactor the team file code. Closes #35, closes #39

--- util/file/common.py
+++ util/file/common.py
@@ -1,5 +1,93 @@
 """A module to organise writing and reading files used by the tool"""
 from . import types
+import os
+import sys
+import math
+
+
+def get_team_files_list(team, teampack):
+    """Collects and allows the user to choose which team files to combine
+
+    Arguments:
+    team - boolean for whether .team files are needed
+    teampack - boolean for whether .teampack files are needed
+    """
+    team_files = []
+    for root, dirs, files in os.walk('.'):
+        for filename in files:
+            if(team and filename[-5:] == '.team'):
+                reldir = os.path.relpath(root, '.')
+                relfile = os.path.join(reldir, filename)
+                if relfile[:2] == './' or relfile[:2] == '.\\':
+                    relfile = relfile[2:]
+                team_files.append(relfile)
+            if(teampack and filename[-9:] == '.teampack'):
+                reldir = os.path.relpath(root, '.')
+                relfile = os.path.join(reldir, filename)
+                if relfile[:2] == './' or relfile[:2] == '.\\':
+                    relfile = relfile[2:]
+                team_files.append(relfile)
+
+    team_files = sorted(team_files)
+    if(len(team_files) == 0):
+        print('No team or teampack files were found.')
+        print('Press Enter to exit.')
+        input('')
+        sys.exit(0)
+
+    return team_files
+
+
+def select_file(files, page=1, mchoice=False):
+    """Allow the user to select a team from a list
+
+    files - the list of files to be chosen from
+    page - which page should be shown initially
+    mchoice - Enables the d - done option.
+              Meant for if the user is able to select
+              multiple choices and decide when they're done.
+    """
+
+    print('')
+
+    cur_page = page
+
+    allowable_options = ['1', '2', '3', '4', '5', '6', '7', '8',
+                         '9', '0', 'n', 'p', 'd']
+    max_pages = math.ceil(len(files)/10)
+    print('Page ' + str(cur_page) + ' of team files')
+    print('Choose an option.')
+    max_range = 10
+    if(cur_page == max_pages):
+        max_range = len(files) % 10
+        if (max_range == 0):
+            max_range = 10
+    for i in range(0, max_range):
+        print(str(i) + '. ' + files[10*(cur_page-1) + i])
+    if (cur_page > 1):
+        print('p. Previous page')
+    if (cur_page < max_pages):
+        print('n. Next page')
+    if (mchoice):
+        print('d. Done')
+    choice = input('--> ').strip()
+    if (choice in allowable_options):
+        if (choice == 'n'):
+            if (cur_page == max_pages):
+                print('You are already on the last page.')
+            else:
+                cur_page += 1
+        elif (choice == 'p'):
+            if (cur_page == 1):
+                print('You are already on the first page.')
+            else:
+                cur_page -= 1
+        elif (choice == 'd'):
+            return (None, cur_page)
+        else:
+            return (files[10*(cur_page-1) + int(choice)], cur_page)
+    else:
+        print('That is not a valid option. Please try again.')
 
 
 def export_file(data, type):
