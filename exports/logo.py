d7bd46f24135697c17072b30350d3f46ccb65a14
Daniel Cole <daniel@judgezarbi.com>
2018-05-13 03:24:29 +0100

    Change references to get_team_guid to the new utility

--- exports/logo.py
+++ exports/logo.py
@@ -33,54 +33,13 @@ def _fetch_data(c, team_guid):
     return data
 
 
-def _get_team_guid(c):
-    """User chooses a team, GUID is returned for use in the DB"""
-    while True:
-        print('Type the name of the team whose logo you wish to export.')
-        team_name = input('--> ')
-        c.execute('SELECT * FROM t_teams WHERE teamName = ? COLLATE NOCASE',
-                  (team_name,))
-        team_choices = c.fetchall()
-        if len(team_choices) == 0:
-            print('No teams exist with the name ' +
-                  team_name +
-                  '. Please try again.')
-        else:
-            break
-    for item in team_choices[:]:
-        if item[1] is not None:
-            team_choices.remove(item)
-
-    if len(team_choices) > 1:
-        while True:
-            print('There are multiple teams with that name, '
-                  'please choose one.')
-            print('Note the highest-numbered teams '
-                  'are the most recently created.')
-            count = 0
-            for item in team_choices:
-                count += 1
-                print(str(count) + '. ' + item[2])
-            try:
-                choice = int(input('--> '))
-            except ValueError:
-                print('That was not a valid number. Please try again.')
-            if (choice < 1 or choice > count):
-                print('That was not a valid choice. Please try again.')
-            else:
-                return team_choices[choice-1][0]
-    else:
-        print('Found a team! Using found team.')
-        return team_choices[0][0]
-
-
 def export_logo():
     """The main function that controls exporting logos"""
 
     try:
         conn = sqlite3.connect('database.sqlite')
         c = conn.cursor()
-        team_guid = _get_team_guid(c)
+        team_guid = util.db._get_team_guid(c)
         data = _fetch_data(c, team_guid)
         conn.close()
     except KeyboardInterrupt:
