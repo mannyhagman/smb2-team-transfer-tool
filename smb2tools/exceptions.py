78c0741f71e69889e5ae3d9ab155052575e20798
Daniel Cole <daniel@judgezarbi.com>
2018-05-30 20:12:44 +0100

    Remove print statements from team-related code, add relevant exceptions

---
+++ smb2tools/exceptions.py
@@ -0,0 +1,17 @@
+class GoBackException(Exception):
+    pass
+
+class NoSaveException(Exception):
+    pass
+
+class TooManySavesException(Exception):
+    pass
+
+class NoExtensionsException(Exception):
+    pass
+
+class NoFilesException(Exception):
+    pass
+
+class IncompatibleException(Exception):
+    pass
---
+++ smb2tools/file/team.py
@@ -0,0 +1,62 @@
+"""A module for importing and exporting team data files"""
+import util
+import json
+import sys
+
+
+def _process_data_ver1(data):
+    data['player_option_data'] = []
+    data['player_colour_data'] = []
+
+    for player in data['player_attr_data']:
+        data['player_option_data'].append([])
+        data['player_colour_data'].append([])
+
+        for item in player:
+            if (item[1] is None):
+                data['player_colour_data'][-1].append([x for x in item
+                                                       if x is not None])
+            else:
+                data['player_option_data'][-1].append([x for x in item
+                                                       if x is not None])
+
+    del data['player_attr_data']
+
+    return data
+
+
+def export_team(data):
+    """Exports the team data to file
+
+    Returns the name of the file that was created.
+
+    Arguments:
+    data - the dictionary to export
+    """
+    team_name = data['team_data'][2]
+
+    fname = util.file.common.get_file_name(team_name,
+                                           util.file.types.FileTypes.TEAM)
+
+    data['version'] = util.file.types.cur_ver
+
+    f = open(fname, 'w')
+    f.write(json.dumps(data, cls=util.json.BytesEncoder))
+    f.close()
+
+    return fname
+
+
+def import_team(file):
+    with open(file) as team_file:
+        data = json.loads(team_file.read(), cls=util.json.BytesDecoder)
+
+    if util.file.common.is_file_compatible(data,
+                                           util.file.types.FileTypes.TEAM):
+        if(util.file.common.get_data_version(data) == 1):
+            return _process_data_ver1(data)
+
+        return data
+    else:
+        raise IncompatibleException
+
