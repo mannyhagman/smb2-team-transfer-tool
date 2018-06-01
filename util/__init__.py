422872c37cb32438f613fc9a57ade5a6eb7569e3
Daniel Cole <daniel@judgezarbi.com>
2018-06-01 12:38:11 +0100

    Move db module out of util, delete empty util module

--- util/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import save  # noqa: F401
-from . import json  # noqa: F401
-from . import file  # noqa: F401
-from . import db  # noqa: F401
-
-
-class GoBackException(Exception):
-    pass
--- util/file/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import team  # noqa: F401
-from . import common  # noqa: F401
-from . import types  # noqa: F401
