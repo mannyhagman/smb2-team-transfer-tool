2562ad6bbf4512f2532d4296e3a6c791e3380ff1
Daniel Cole <daniel@judgezarbi.com>
2018-06-02 15:21:03 +0100

    Put a couple of utility modules together in one file

--- util.py
+++ util.py
@@ -71,3 +71,44 @@ def select_file(files, page=1, mchoice=False, all=False):
                 return (files[10*(cur_page-1) + int(choice)], cur_page)
         else:
             print('That is not a valid option. Please try again.')
+
+
+def _get_team_guid(c):
+    """User chooses a team, GUID is returned for use in the DB"""
+    while True:
+        team_name = input('--> ')
+        c.execute('SELECT * FROM t_teams WHERE teamType = 1 AND '
+                  ' teamName = ? COLLATE NOCASE',
+                  (team_name,))
+        team_choices = c.fetchall()
+        if len(team_choices) == 0:
+            print('No teams exist with the name ' +
+                  team_name +
+                  '. Please try again.')
+        else:
+            break
+    for item in team_choices[:]:
+        if item[1] is not None:
+            team_choices.remove(item)
+
+    if len(team_choices) > 1:
+        while True:
+            print('There are multiple teams with that name, '
+                  'please choose one.')
+            print('Note the highest-numbered teams '
+                  'are the most recently created.')
+            count = 0
+            for item in team_choices:
+                count += 1
+                print(str(count) + '. ' + item[2])
+            try:
+                choice = int(input('--> '))
+            except ValueError:
+                print('That was not a valid number. Please try again.')
+            if (choice < 1 or choice > count):
+                print('That was not a valid choice. Please try again.')
+            else:
+                return team_choices[choice-1][0]
+    else:
+        print('Found a team! Using found team.')
+        return team_choices[0][0]
